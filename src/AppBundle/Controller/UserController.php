071d37930460f1dc7a2cdb104c47874bfc9dd28a
Augustin Leconte <augustin.leconte@epitech.eu>
2020-12-15 14:31:43 +0100

    add signup route

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,64 +3,81 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
+use JMS\Serializer\SerializationContext;
 
 class UserController extends Controller
 {
-    /**
-     * @Route("/users/getUser/{id}", name="user_show")
-		 * @Method({"GET"})
-     */
-    public function getUserInfos()
-    {
-        $user = new User();
-        $user->setName('test User');
-        $user->setPassword('azerty1234');
 
-        $data =  $this->get('serializer')->serialize($user, 'json');
+	/**
+	* @Route("/users/getUsers", name="users_show")
+	* @Method({"GET"})
+	*/
+	public function getUsers()
+	{
+		$articles = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+		$data = $this->get('jms_serializer')->serialize($articles, 'json', SerializationContext::create()->setGroups(array('list')));
 
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
 
-        return $response;
-    }
+		return $response;
+	}
 
-		/**
-		 * @Route("/users/signin", name="user_get")
-		 * @Method({"POST"})
-		 */
-		public function getUserID()
-		{
-				$user = new User();
-				$user->setName('test User');
-				$user->setPassword('azerty1234');
+	/**
+	* @Route("/users/getUser/{id}", name="user_show")
+	* @Method({"GET"})
+	*/
+	public function getUserInfos()
+	{
+		$user = new User();
+		$user->setName('test User');
+		$user->setPassword('azerty1234');
 
-				$data =  $this->get('serializer')->serialize($user, 'json');
+		$data =  $this->get('serializer')->serialize($user, 'json');
 
-				$response = new Response($data);
-				$response->headers->set('Content-Type', 'application/json');
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
 
-				return $response;
-		}
+		return $response;
+	}
 
-		/**
-     * @Route("/users/signup", name="user_create")
-     * @Method({"POST"})
-     */
-    public function createUser(Request $request)
-    {
-        $data = $request->getContent();
-        $user = $this->get('serializer')->deserialize($data, 'AppBundle\Entity\User', 'json');
+	/**
+	* @Route("/users/signin", name="user_get")
+	* @Method({"POST"})
+	*/
+	public function getUserID()
+	{
+		$user = new User();
+		$user->setName('test User');
+		$user->setPassword('azerty1234');
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($user);
-        $em->flush();
+		$data =  $this->get('serializer')->serialize($user, 'json');
 
-        return new Response('', Response::HTTP_CREATED);
-    }
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+
+		return $response;
+	}
+
+	/**
+	* @Route("/users/signup", name="user_create")
+	* @Method({"POST"})
+	*/
+	public function createUser(Request $request)
+	{
+		$data = $request->getContent();
+		$user = $this->get('serializer')->deserialize($data, 'AppBundle\Entity\User', 'json');
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($user);
+		$em->flush();
+
+		return new Response('', Response::HTTP_CREATED);
+	}
 
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,47 +3,54 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
 
 /**
- * @ORM\Entity
- * @ORM\Table(name="user")
- */
+* @ORM\Entity
+* @ORM\Table(name="user")
+*/
 class User
 {
-    /**
-     * @ORM\Column(type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+	/**
+	* @ORM\Column(type="integer")
+	* @ORM\Id
+	* @ORM\GeneratedValue(strategy="AUTO")
+	*
+	* @Serializer\Groups({"list"})
+	*/
+	private $id;
 
-    /**
-    * @ORM\Column(type="text")
-    */
-    private $name;
+	/**
+	* @ORM\Column(type="text")
+	*
+	* @Serializer\Groups({"detail", "list"})
+	*/
+	private $name;
 
-    /**
-    * @ORM\Column(type="text")
-    */
-    private $password;
+	/**
+	* @ORM\Column(type="text")
+	*
+	* @Serializer\Groups({"detail", "list"})
+	*/
+	private $password;
 
-    public function getName()
-    {
-        return $this->name;
-    }
+	public function getName()
+	{
+		return $this->name;
+	}
 
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
 
-    public function getPassword()
-    {
-        return $this->password;
-    }
+	public function getPassword()
+	{
+		return $this->password;
+	}
 
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
 }
